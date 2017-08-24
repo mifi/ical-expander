aff306ed7161b39a968ab0b4f44da3c609dab98e
Hauke Pribnow <hauke.pribnow@gmx.net>
2017-08-24 20:12:33 +0200

    made ical-expander a Universal JavaScript Module / implemented Universal Module Definition pattern

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 'use strict';
 
-const ICAL = require('ical.js');
+(function(global, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define([ 'ical.js' ], factory);
+	}
+	else if (typeof exports === 'object') { // Node/CommonJS
+		module.exports = factory(require('ical.js'));
+	}
+	else {
+		global.IcalExpander = factory(ICAL);
+	}
+})(this, function(ICAL) {
 
 // Copied from https://dxr.mozilla.org/comm-central/source/calendar/timezones/zones.json
 // And compiled using node compile-zones.js
@@ -128,4 +138,6 @@ function registerTimezones() {
 
 registerTimezones();
 
-module.exports = IcalExpander;
+return IcalExpander;
+
+});
